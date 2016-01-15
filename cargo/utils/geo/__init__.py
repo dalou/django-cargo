088819262e56b1c618ad43273f569fe63c0f5461
Dalou <autrusseau.damien@gmail.com>
2016-01-15 12:13:05 +0100

    fix

--- cargo/utils/geo/__init__.py
+++ cargo/utils/geo/__init__.py
@@ -1,14 +1,28 @@
+import re
+
+from django.conf import settings
 
 from gmaps import Geocoding
 from gmaps.errors import NoResults, GmapException, InvalidRequest
 
-def geocode(address):
+latlngreg = re.compile(r'\@([-+]?\d*\.\d+|\d+)\,([-+]?\d*\.\d+|\d+)')
 
-    api = Geocoding()
+def geocode(address, api_key=None):
+
+    api_key = getattr(settings, 'CARGO_GOOGLE_API_KEY', api_key)
+    if api_key and api_key.strip() == "":
+        api_key = None
+
+    api = Geocoding(api_key=api_key)
     if address.strip() != '':
         original_address = address.strip()
         try:
-            results = api.geocode(original_address)
+
+            latlng = latlngreg.search(original_address)
+            if latlng:
+                results = api.reverse(float(latlng.group(1)), float(latlng.group(2)))
+            else:
+                results = api.geocode(original_address)
 
             if len(results):
                 results[0]['lng'] = results[0].get('geometry', {}).get('location', {}).get('lng', None)
